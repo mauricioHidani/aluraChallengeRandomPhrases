4091b592cda6efb9ff866e5b0df2220f25467740
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-11-05 08:21:00 -0300

    init challenge random phrases

---
+++ src/main/java/br/com/challenge/alura/phrases/random/RandomPhrasesApplication.java
@@ -0,0 +1,11 @@
+package br.com.challenge.alura.phrases.random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPhrasesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPhrasesApplication.class, args);
+	}
+}
